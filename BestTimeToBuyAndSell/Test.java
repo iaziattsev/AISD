c392188a399062db1c7444bb6f682d75718deedf
iaziattsev <90763434+iaziattsev@users.noreply.github.com>
2022-05-18 14:07:01 +0300

    Delete BestTimeToBuyAndSell directory

--- BestTimeToBuyAndSell/Test.java
+++
@@ -1,8 +0,0 @@
-package BestTimeToBuyAndSell;
-
-public class Test extends Solution {
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        //s.maxProfit(7,1,5,3,6,4);
-    }
-}
